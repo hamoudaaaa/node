db0c26154e453207ab14995be8048639e608551f
Asma <ahmedbenmiled98@gmail.com>
2021-04-14 08:54:18 +0100

    fist node

---
+++ app.js
@@ -0,0 +1,3 @@
+const ahmed = require("./mathlib");
+console.log(ahmed.multiplu(15, 80));
+console.log(ahmed.sum(15, 80));
---
+++ mathlib.js
@@ -0,0 +1,8 @@
+const sum = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const multiplu = (a, b) => a * b;
+
+module.exports = {
+    multiplu,
+    sum,
+};
